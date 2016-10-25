dfd4dd53c315228c6dce8a3b2fa0369afebb9df6
Sreeraj Thottipparambil <sreeraj.thottipparambil@oracle.com>
2016-10-25 17:28:58 +0530

    Page template for nmain objects

---
+++ ViewController/src/sfa/view/LoginBean.java
@@ -0,0 +1,27 @@
+package sfa.view;
+
+public class LoginBean {
+    private String username;
+    private String password;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    
+    private String doLogin()
+    {
+        return null;
+    }
+}
