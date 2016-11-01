f82fab64cbbb442e24b2185eaea9ec66d12f7757
Adil Muthukoya <adil.muthukoya@oracle.com>
2016-11-01 18:33:54 +0530

    Created a Contact Task flow

---
+++ SfaModel/src/com/sfa/model/view/common/ContactAddressViewSDO.java
@@ -0,0 +1,79 @@
+package com.sfa.model.view.common;
+
+public interface ContactAddressViewSDO extends java.io.Serializable {
+
+   public java.math.BigInteger getAccountId();
+
+   public void setAccountId(java.math.BigInteger value);
+
+   public java.lang.Integer getAddressId1();
+
+   public void setAddressId1(java.lang.Integer value);
+
+   public java.lang.Integer getContactId();
+
+   public void setContactId(java.lang.Integer value);
+
+   public java.math.BigInteger getCreatedBy();
+
+   public void setCreatedBy(java.math.BigInteger value);
+
+   public java.sql.Timestamp getCreationDate();
+
+   public void setCreationDate(java.sql.Timestamp value);
+
+   public java.math.BigInteger getDepartmentId();
+
+   public void setDepartmentId(java.math.BigInteger value);
+
+   public java.lang.String getDescription();
+
+   public void setDescription(java.lang.String value);
+
+   public java.lang.String getEmail();
+
+   public void setEmail(java.lang.String value);
+
+   public java.lang.String getFirstName();
+
+   public void setFirstName(java.lang.String value);
+
+   public java.lang.Integer getInfluenceLevel();
+
+   public void setInfluenceLevel(java.lang.Integer value);
+
+   public java.lang.String getIsPrimaryContact();
+
+   public void setIsPrimaryContact(java.lang.String value);
+
+   public java.lang.String getLastName();
+
+   public void setLastName(java.lang.String value);
+
+   public java.lang.String getMiddleName();
+
+   public void setMiddleName(java.lang.String value);
+
+   public java.lang.String getMobilePhone();
+
+   public void setMobilePhone(java.lang.String value);
+
+   public java.math.BigInteger getModifiedBy();
+
+   public void setModifiedBy(java.math.BigInteger value);
+
+   public java.sql.Timestamp getModifiedDate();
+
+   public void setModifiedDate(java.sql.Timestamp value);
+
+   public java.lang.String getTitle();
+
+   public void setTitle(java.lang.String value);
+
+   public java.lang.String getWorkPhone();
+
+   public void setWorkPhone(java.lang.String value);
+
+
+}
+
---
+++ SfaModel/src/com/sfa/model/view/common/ContactAddressViewSDOImpl.java
@@ -0,0 +1,5 @@
+package com.sfa.model.view.common;
+
+import org.eclipse.persistence.sdo.SDODataObject;
+
+
